2401cd2094b555083a587733fd3fb038d6876996
johntymuriithi <muriithijohn634@gmail.com>
2024-01-15 21:12:27 +0300

    tried to solve the classes bugs

--- models/state.py
+++ models/state.py
@@ -8,22 +8,26 @@ from datetime import datetime
 
 class State(BaseModel):
     """State class that inherits from BaseModel"""
-
+    
+    name = ""
+    
     def __init__(self, *args, **kwargs):
-        """Initialization function that's called when a new instance
-        of the class is created"""
-        from models import storage
+        super().__init__(**kwargs)
+    # def __init__(self, *args, **kwargs):
+    #     """Initialization function that's called when a new instance
+    #     of the class is created"""
+    #     from models import storage
 
-        if kwargs and len(kwargs) > 0:
-            # Iterate through the dictionary and get the key/value pairs
-            for key, value in kwargs.items():
-                if key != "__class__":
-                    if key == "created_at" or key == "updated_at":
-                        setattr(self, key, datetime.fromisoformat(value))
-                    else:
-                        setattr(self, key, value)
-        else:
-            super().__init__()
-            self.name = ""
+    #     if kwargs and len(kwargs) > 0:
+    #         # Iterate through the dictionary and get the key/value pairs
+    #         for key, value in kwargs.items():
+    #             if key != "__class__":
+    #                 if key == "created_at" or key == "updated_at":
+    #                     setattr(self, key, datetime.fromisoformat(value))
+    #                 else:
+    #                     setattr(self, key, value)
+    #     else:
+    #         super().__init__()
+    #         self.name = ""
 
-            storage.new(self)
+    #         storage.new(self)
