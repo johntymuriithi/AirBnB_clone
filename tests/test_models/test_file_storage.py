f9df3156abf15970c37280b0e698f13425c5455c
johntymuriithi <muriithijohn634@gmail.com>
2024-01-15 20:30:19 +0300

    looking at the test for filestorage

---
+++ tests/test_models/test_file_storage.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""Write tests for the file storage t"""
+import unittest
+import uuid
+from datetime import datetime
+from models import storage
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+
+
+class TestFileStorage(unittest.TestCase):
+    """Defines a class TESTFIELSTORAGE"""
+    def setUp(self):
+        """Sets up all instances needed fo this test"""
+        self.baseModal = BaseModel()
+        self.storage = FileStorage()
+    
+    # def tearDown(self):
+    #     """clean up test files"""
+    #     FileStorage.__FileStorage__objects = {}
+    def test_all(self):
+        result = self.storage.all()
+        self.assertEqual(type(result), dict)
+    
+    
