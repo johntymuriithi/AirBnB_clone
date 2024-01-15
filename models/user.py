58e2b32bc4551346927a657a0cd08631310b023a
johntymuriithi <muriithijohn634@gmail.com>
2024-01-15 21:02:38 +0300

    looking at the test for filestorage

--- models/user.py
+++ models/user.py
@@ -9,28 +9,35 @@ import uuid
 
 class User(BaseModel):
     """User class that inherits from BaseModel"""
-
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+    
     def __init__(self, *args, **kwargs):
-        """Initialization function that's called when a new instance
-        of the class is created"""
-        from models import storage
+        super().__init__(**kwargs)
 
-        if kwargs and len(kwargs) > 0:
-            # Iterate through the dictionary and get the key/value pairs
-            for key, value in kwargs.items():
-                if key != "__class__":
-                    if key == "created_at" or key == "updated_at":
-                        setattr(self, key, datetime.fromisoformat(value))
-                    else:
-                        setattr(self, key, value)
-        else:
-            super().__init__()
-            # self.id = str(uuid.uuid4())
-            # self.created_at = datetime.now()
-            # self.updated_at = datetime.now()
-            self.email = ""
-            self.password = ""
-            self.first_name = ""
-            self.last_name = ""
+    # def __init__(self, *args, **kwargs):
+    #     """Initialization function that's called when a new instance
+    #     of the class is created"""
+    #     from models import storage
 
-            storage.new(self)
+    #     if kwargs and len(kwargs) > 0:
+    #         # Iterate through the dictionary and get the key/value pairs
+    #         for key, value in kwargs.items():
+    #             if key != "__class__":
+    #                 if key == "created_at" or key == "updated_at":
+    #                     setattr(self, key, datetime.fromisoformat(value))
+    #                 else:
+    #                     setattr(self, key, value)
+    #     else:
+    #         super().__init__()
+    #         # self.id = str(uuid.uuid4())
+    #         # self.created_at = datetime.now()
+    #         # self.updated_at = datetime.now()
+    #         self.email = ""
+    #         self.password = ""
+    #         self.first_name = ""
+    #         self.last_name = ""
+
+    #         storage.new(self)
