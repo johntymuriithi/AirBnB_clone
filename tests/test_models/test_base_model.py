9c508e5e8df2b336bbb822063f379d22a5bdc233
johntymuriithi <muriithijohn634@gmail.com>
2024-01-16 22:02:03 +0300

    commit

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -63,7 +63,7 @@ class TestBaseModel(unittest.TestCase):
         expected = "[{}] ({}) {}".format(instance.__class__.__name__,
                                        instance.id, instance.__dict__)
         self.assertEqual(expected, str(instance))
-        self.assertIsInstance(instance.__str__, str)
+        # self.assertIsInstance(instance.__str__, str)
     
     # def test_str(self):
     #     instance = BaseModel()
